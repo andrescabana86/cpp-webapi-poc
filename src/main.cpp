083a527ae878c7ba6bfdb85c1913d228a32c66cb
andrescabana86 <andrescabana86@gmail.com>
2023-03-09 20:56:49 -0300

    feat: use docker into project *  add index files *  add html files and static files

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,63 @@
 //
 #include "crow_all.h"
 
+void sendFile(crow::response &res, std::string filename, std::string contentType) {
+    std::ifstream in("../public/" + filename, std::ifstream::in);
+    if (!in.fail()) {
+        std::ostringstream contents;
+        contents << in.rdbuf();
+        in.close();
+        res.set_header("Content-Type", contentType);
+        res.write(contents.str());
+    } else {
+        res.code = 404;
+        res.write("Not Found");
+    }
+    res.end();
+}
+
+void sendHtml(crow::response &res, std::string filename) {
+    sendFile(res, filename + ".html", "text/html");
+}
+
+
+void sendImage(crow::response &res, std::string filename) {
+    sendFile(res, "image/" + filename, "image/jpeg");
+}
+
+
+void sendScript(crow::response &res, std::string filename) {
+    sendFile(res, "script/" + filename, "text/javascript");
+}
+
+
+void sendStyle(crow::response &res, std::string filename) {
+    sendFile(res, "style/" + filename, "text/css");
+}
+
 int main() {
     crow::SimpleApp app;
+
     CROW_ROUTE(app, "/")
-            ([]() {
-                return "<div>Hellow world!</div>";
+            ([](const crow::request &req, crow::response &res) {
+                sendHtml(res, "index");
+            });
+    CROW_ROUTE(app, "/style/<string>")
+            ([](const crow::request &req, crow::response &res, std::string filename) {
+                sendStyle(res, filename);
+            });
+    CROW_ROUTE(app, "/script/<string>")
+            ([](const crow::request &req, crow::response &res, std::string filename) {
+                sendScript(res, filename);
+            });
+    CROW_ROUTE(app, "/image/<string>")
+            ([](const crow::request &req, crow::response &res, std::string filename) {
+                sendImage(res, filename);
             });
 
     char* port = getenv("PORT");
     uint16_t iPort = static_cast<uint16_t>(port != NULL ? std::stoi(port) : 18080);
     std::cout << "PORT = " << iPort << std::endl;
-
     app.port(iPort).multithreaded().run();
 
     return 0;
