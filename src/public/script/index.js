5206d8fadb1b0ca9a9dea889ce93cfe9bd92367a
andrescabana86 <andrescabana86@gmail.com>
2023-03-09 21:11:48 -0300

    feat: use docker into project *  compile react on browser

--- src/public/script/index.js
+++ src/public/script/index.js
@@ -1,1 +1,15 @@
-console.log('i am a script');
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				this is a React app example
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById("root")
+);
+
